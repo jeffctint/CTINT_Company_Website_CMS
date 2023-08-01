f4c1fa3db1f7159e6f0b3bfe6c07118e1eff9966
Jeff Lam (CTHK) <jeff.lam@ctint.com>
2023-08-01 10:05:06 +0800

    fixed upload images problem

--- server/src/app.ts
+++ server/src/app.ts
@@ -23,9 +23,9 @@ app.use(helmet());
 // prevent http param pollution
 app.use(hpp());
 // parse json request body
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Sanitize request data
 
 // parse cookie
